9b4410aabbe33bbb8a52864e8e34fd4515f57201
TayfurSafakGencay <safak.gencay@gmail.com>
2024-03-02 12:59:20 +0300

    New: Shooting

---
+++ Assets/Scripts/Contexts/Main/View/Tank/Shooting/ShootingMediator.cs
@@ -0,0 +1,30 @@
+﻿using strange.extensions.mediation.impl;
+using UnityEngine;
+
+namespace Contexts.Main.View.Tank.Shooting
+{
+  public class ShootingMediator : EventMediator
+  {
+    [Inject]
+    public ShootingView view { get; set; }
+
+    public override void OnRegister()
+    {
+    }
+
+    private void Update()
+    {
+      if (!Input.GetKeyDown(KeyCode.Mouse0)) return;
+      
+      view.BulletPool[view.Queue].SetActive(true);
+      view.Queue++;
+      
+      if (view.Queue != 20) return;
+      view.Queue = 0;
+    }
+
+    public override void OnRemove()
+    {
+    }
+  }
+}
---
+++ Assets/Scripts/Contexts/Main/View/Tank/Shooting/ShootingView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Contexts.Main.View.Tank.Bullet;
+using strange.extensions.mediation.impl;
+using UnityEngine;
+
+namespace Contexts.Main.View.Tank.Shooting
+{
+  public class ShootingView : EventView
+  {
+    public Transform Muzzle;
+
+    public GameObject Bullet;
+
+    public Transform BulletsParent;
+    
+    public int BulletCount = 20;
+    
+    [HideInInspector]
+    public List<GameObject> BulletPool;
+
+    [HideInInspector]
+    public int Queue;
+
+    protected override void Start()
+    {
+      Transform muzzleTransform = Muzzle.transform;
+
+      for (int i = 0; i < BulletCount; i++)
+      {
+        GameObject bulletObject = Instantiate(Bullet, muzzleTransform.position, muzzleTransform.rotation, BulletsParent);
+        bulletObject.SetActive(false);
+        bulletObject.GetComponent<BulletBehaviour>().SetMuzzle(Muzzle);
+        BulletPool.Add(bulletObject);
+      }
+    }
+  }
+}
